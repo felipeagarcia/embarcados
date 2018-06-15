a664db217db874f237cd631f4fb236867f1cf7fd
MarcoSchaefer <marcoatschaefer@gmail.com>
2018-06-15 14:47:28 -0300

    test

--- test.c
+++ test.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define COL_NUM 2
-#define ROW_NUM 3
-
-float data[3][COL_NUM] = {{1,2},{3,4},{5,6}};
+#include <system.h>
 
 
-int main(){
-  int i,j;
-  for(i=0;i<ROW_NUM;i++){
-    for(j=0;j<COL_NUM;j++){
-      printf("[%f]",data[i][j]);
-    }
-    printf("\n");
+int main()
+  int i;
+  for(i=0;i<10;i++){
+    printf("[%d][%d]\n",10*i*i);
   }
 
   return 0;
